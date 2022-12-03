59722a9f5a1f98c1132551dacb5b52e9e937ecaa
SinghDinesh <SinghDinesh@User>
2022-12-03 22:29:42 +0530

    Second Commit

---
+++ ActiTimeMaven/src/test/java/com/actitime/testScript/TaskModule.java
@@ -0,0 +1,19 @@
+package com.actitime.testScript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TaskModule {
+@Test
+public void CreateTask() {
+	Reporter.log("CreateTask",true);
+}
+@Test
+public void ModifyTask() {
+	Reporter.log("ModifyTask",true);
+}
+@Test
+public void DeleteTask() {
+	Reporter.log("DeleteTask",true);
+}
+}
